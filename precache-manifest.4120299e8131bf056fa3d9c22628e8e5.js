2a5271383406f022ca14ccb8188f2cc0d7f1f5f6
y0on2q <ltnscp9028@gmail.com>
2020-05-20 18:16:56 +0900

    Updates

--- precache-manifest.4120299e8131bf056fa3d9c22628e8e5.js
+++ precache-manifest.4120299e8131bf056fa3d9c22628e8e5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9fa0a2ba56bfa255ac2a905a08aec8ee",
+    "revision": "2e1e1b4d337cd1a50f85971f37c87651",
     "url": "/maple-helper/index.html"
   },
   {
-    "revision": "9eb2d69df0a906bdfa37",
-    "url": "/maple-helper/static/css/main.30d23eb8.chunk.css"
+    "revision": "df0f6c730195ac2fb4f5",
+    "url": "/maple-helper/static/css/main.37fe6a3c.chunk.css"
   },
   {
-    "revision": "33880350d881700ee690",
-    "url": "/maple-helper/static/js/2.21c89b35.chunk.js"
+    "revision": "ddb01635d4c3665dc819",
+    "url": "/maple-helper/static/js/2.c3bc93d1.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/maple-helper/static/js/2.21c89b35.chunk.js.LICENSE.txt"
+    "url": "/maple-helper/static/js/2.c3bc93d1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9eb2d69df0a906bdfa37",
-    "url": "/maple-helper/static/js/main.73384c0e.chunk.js"
+    "revision": "df0f6c730195ac2fb4f5",
+    "url": "/maple-helper/static/js/main.6693fdfc.chunk.js"
   },
   {
     "revision": "75d39103eff29dedb6bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maple-helper/precache-manifest.1e6dac1571fbe111f56da79bd8b11d55.js"
+  "/maple-helper/precache-manifest.4120299e8131bf056fa3d9c22628e8e5.js"
 );
 
 self.addEventListener('message', (event) => {
